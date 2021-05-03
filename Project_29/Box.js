eae39a9fd054b7f058a120b763706379896d1539
TrishaBhatia <73651359+TrishaBhatia@users.noreply.github.com>
2021-05-03 11:05:18 +0530

    Add files via upload

---
+++ Project_29/Box.js
@@ -0,0 +1,27 @@
+class Box{
+  constructor(x, y) {
+    var options = {
+        'restitution':0.4,
+        'friction':0.0
+       
+    }
+    this.body = Bodies.rectangle(x, y, 30, 40, options);
+    this.width = 30;
+    this.height = 40;
+    //this.image = loadImage("sprites/base.png");
+    World.add(world, this.body);
+  }
+  display(){
+    var angle = this.body.angle;
+    push();
+    translate(this.body.position.x, this.body.position.y);
+    rotate(angle);
+    //imageMode(CENTER);
+    //image(this.image, 0, 0, this.width, this.height);
+    
+    rect(0, 0, this.width, this.height);
+    pop();
+  }
+  
+  };
+  
